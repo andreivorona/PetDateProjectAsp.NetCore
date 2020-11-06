325407a80d3aeaa9579b82545ef3e85ce1761a23
andreivorona <andreivorona@abv.bg>
2020-11-06 11:08:23 +0200

    First Commit of the project.Basic configuration for Mvc.Have migration for User and Views for Users.Nothing changed yet.

---
+++ PetsDate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PetsDate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

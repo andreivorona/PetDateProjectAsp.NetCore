0ad69222086e5349d086d817584ee1247bd1c08a
andreivorona <andreivorona@abv.bg>
2020-11-09 22:06:04 +0200

    Add scss to project and implement own styles.Ignore default site.css.Add test pages.

---
+++ PetsDate/Controllers/UserpageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetsDate.Controllers
+{
+    public class UserpageController : Controller
+    {
+        //[Authorize]
+        public IActionResult Collection()
+        {
+            return this.View();
+        }
+
+        //[Authorize]
+        public IActionResult CreateCard()
+        {
+            return this.View();
+        }
+    }
+}
